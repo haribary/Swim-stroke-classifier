edfc8814c7efd4d9c2085e4a80e638135213c1e6
Harrison Li <liharrison00@Gmail.com>
2025-09-23 01:37:22 -0700

    visualization+ViT

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+from pprint import pprint
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 def extract_frames(path_video,path_images,fps=5):
     """Takes mp4 video and saves it as a folder of images of each frame"""
     vidcap = cv2.VideoCapture(path_video)
@@ -16,7 +19,61 @@ def extract_frames(path_video,path_images,fps=5):
         frames+=1
     vidcap.release()
 
+strokes=['Butterfly','Backstroke','Breaststroke','Freestyle']
+views=['Aerial','Front','Side_above_water','Side_underwater','Side_water_level']
+bodies=['Swimmer_Skin_0,25_Muscle_2','Swimmer_Skin_0,25_Muscle_8','Swimmer_Skin_0,75_Muscle_2','Swimmer_Skin_0,75_Muscle_8']
+waters=['Water_Quantity_0,25_Height_0,6','Water_Quantity_0,25_Height_1','data/raw/labels/Butterfly/Aerial/Swimmer_Skin_0,25_Muscle_2/Water_Quantity_0,25_Height_1,5','Water_Quantity_0,75_Height_0,6','Water_Quantity_0,75_Height_1','Water_Quantity_0,75_Height_1,5']
+lightings=['Lighting_rotx_110_roty_190','Lighting_rotx_110_roty_280','Lighting_rotx_110_roty_360','Lighting_rotx_140_roty_190','Lighting_rotx_140_roty_280','Lighting_rotx_140_roty_360']
+speeds=['Speed_2','Speed_3']
+positions=['position_1,75','position_3,75']
+formats=['base','body25','COCO']
+views=['2D_cam','2D_pelvis','3D_cam','3D_pelvis']
 
-path_video="C:/Users/lihar/Code/swim-stroke-classifier/data/raw/5 Second Countdown HD.mp4"
-path_image="C:/Users/lihar/Code/swim-stroke-classifier/data/processed"
-extract_frames(path_video,path_image)
+def take_line(path,idx):
+    with open(path,'r') as file:
+        lines = file.readlines()
+        line = lines[idx]
+        line = line.replace(',','.').split(';')[:-1]
+        frame=[[],[]]
+        for i in range(0, len(line), 3):
+            x = float(line[i])
+            y = float(line[i+1])
+            # ignore confidence: line[j+2]
+            frame[0].append(x)
+            frame[1].append(y)
+
+        return np.array(frame)
+        # print(frame)
+        # print(len(frame))
+
+
+def parse_data():
+    pass
+
+path_data = 'data/raw/labels/Backstroke/Aerial/Swimmer_Skin_0,25_Muscle_2/Water_Quantity_0,25_Height_0,6/Lighting_rotx_110_roty_190/Speed_2/position_1,75/COCO/2D_cam.txt'
+
+def plot_animation(path_data):
+    fig, ax = plt.subplots()
+    ax.set_xlim(0, 2000)
+    ax.set_ylim(0, 800)
+    frame0 = take_line(path_data, 1)
+    scatter = ax.scatter(frame0[0], frame0[1])
+    def update(i):
+        frame = take_line(path_data, i)
+        scatter.set_offsets(np.c_[frame[0], frame[1]])
+        return scatter,
+    ani=FuncAnimation(fig,update,frames=range(1, 200),interval=50)
+
+    plt.show()
+
+plot_animation(path_data)
+
+# path_video="C:/Users/lihar/Code/swim-stroke-classifier/data/raw/5 Second Countdown HD.mp4"
+# path_image="C:/Users/lihar/Code/swim-stroke-classifier/data/processed"
+# #extract_frames(path_video,path_image)
+
+# data = np.load("C:/Users/lihar/Code/swim-stroke-classifier/data/raw/freestyle/freestyle_1_poses.npz")
+# print('arrays in file',data.files)
+# poses = data['poses']
+# print(poses.shape)
+# print(poses[0])
--- src/pose_estimation.py
+++ src/pose_estimation.py
@@ -1,6 +1,12 @@
 import cv2
 import mediapipe as mp
-def run_pose_estimation(frame,out_path):
+import requests
+import numpy as np
+import torch
+from PIL import Image
+import supervision as sv
+from transformers import AutoProcessor, RTDetrForObjectDetection, VitPoseForPoseEstimation, infer_device
+def run_pose_estimation_mediapipe(frame,out_path):
     """"""
     mpPose = mp.solutions.pose
     mpDraw = mp.solutions.drawing_utils
@@ -16,11 +22,80 @@ def run_pose_estimation(frame,out_path):
         return result.pose_landmarks
     print("no pose detected")
     return None
+def run_pose_estimation_vitpose(frame,out_path):
+    device = infer_device()
+    image = Image.open(frame)
+
+    # Detect humans in the image
+    person_image_processor = AutoProcessor.from_pretrained("PekingU/rtdetr_r50vd_coco_o365")
+    person_model = RTDetrForObjectDetection.from_pretrained("PekingU/rtdetr_r50vd_coco_o365", device_map=device)
+
+    inputs = person_image_processor(images=image, return_tensors="pt").to(person_model.device)
+
+    with torch.no_grad():
+        outputs = person_model(**inputs)
+
+    results = person_image_processor.post_process_object_detection(
+        outputs, target_sizes=torch.tensor([(image.height, image.width)]), threshold=0.3
+    )
+    result = results[0]
+
+    # Human label refers 0 index in COCO dataset
+    person_boxes = result["boxes"][result["labels"] == 0]
+    person_boxes = person_boxes.cpu().numpy()
+
+    # Convert boxes from VOC (x1, y1, x2, y2) to COCO (x1, y1, w, h) format
+    person_boxes[:, 2] = person_boxes[:, 2] - person_boxes[:, 0]
+    person_boxes[:, 3] = person_boxes[:, 3] - person_boxes[:, 1]
+
+    # Detect keypoints for each person found
+    image_processor = AutoProcessor.from_pretrained("usyd-community/vitpose-base-simple")
+    model = VitPoseForPoseEstimation.from_pretrained("usyd-community/vitpose-base-simple", device_map=device)
+
+    inputs = image_processor(image, boxes=[person_boxes], return_tensors="pt").to(model.device)
+
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    pose_results = image_processor.post_process_pose_estimation(outputs, boxes=[person_boxes])
+    image_pose_result = pose_results[0]
+
+    xy = torch.stack([pose_result['keypoints'] for pose_result in image_pose_result]).cpu().numpy()
+    scores = torch.stack([pose_result['scores'] for pose_result in image_pose_result]).cpu().numpy()
+
+    key_points = sv.KeyPoints(
+        xy=xy, confidence=scores
+    )
+
+    edge_annotator = sv.EdgeAnnotator(
+        color=sv.Color.GREEN,
+        thickness=1
+    )
+    vertex_annotator = sv.VertexAnnotator(
+        color=sv.Color.RED,
+        radius=2
+    )
+    annotated_frame = edge_annotator.annotate(
+        scene=image.copy(),
+        key_points=key_points
+    )
+    annotated_frame = vertex_annotator.annotate(
+        scene=annotated_frame,
+        key_points=key_points
+    )
+    #annotated_frame.save(out_path+'/ViTtest1.jpg')
+    w,h=image.size
+    print(f'Width={w}, Height={h}')
+    return xy[0]
+
 
 def pose_on_all():
     pass
 
 
-out_path="C:/Users/lihar/Code/swim-stroke-classifier/data/processed"
-frame="C:/Users/lihar/Code/swim-stroke-classifier/data/raw/WIN_20250919_00_19_59_Pro.jpg"
-run_pose_estimation(frame,out_path)
+# out_path="C:/Users/lihar/Code/swim-stroke-classifier/data/processed"
+# frame="C:/Users/lihar/Code/swim-stroke-classifier/data/raw/WIN_20250919_00_19_59_Pro.jpg"
+# run_pose_estimation_mediapipe(frame,out_path)
+frame="C:/Users/lihar/Code/swim-stroke-classifier/data/raw/WIN_20250922_22_37_57_Pro.jpg"
+out_path='C:/Users/lihar/Code/swim-stroke-classifier/data/processed'
+run_pose_estimation_vitpose(frame,out_path)
